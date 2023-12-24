c47338e6ae49c0a6e1737dd701b9affa6608954e
vadimsoude <vadimsoude@gmail.com>
2023-12-24 19:23:49 +0100

    Addition of a controller for the products

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkshopAPI.Models;
+
+namespace WorkshopAPI.Controllers;
+
+[Route("api/product")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly bdeContext _context;
+    
+    public ProductController(bdeContext context)
+    {
+        _context = context;
+    }
+    
+    
+    // Post: api/product/boisson/all
+    [HttpGet("boisson/all")]
+    public async Task<ActionResult<IEnumerable>> getAllBoisson()
+    {
+        var products = await _context.Produits
+            .Where(products => products.Categorie == "BOISSON")
+            .ToListAsync();
+        return products;
+    }
+}
